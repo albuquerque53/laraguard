176794f5abaa6beeb09c63570e3cb8bc980f4cd3
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-20 17:32:30 -0300

    :twisted_rightwards_arrows: Dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'auth.login')->name('login.form');
+Route::post('/', [
+    LoginController::class, 'doLogin'
+    ])->name('login');
+
+Route::get('/dashboard', function() {
+    return view('dashboard.dash');
+})->middleware('auth:web');
