3c63d467cbc09dbb22d6abb640433c01918af6bf
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-20 14:50:29 -0300

    :page_facing_up: Login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,18 @@
+@extends('auth.master.master')
+
+@section('title', 'Login')
+
+@section('content')
+
+<div class="main">
+
+    <p class="sign" align="center">Sign in</p>
+    <form class="form1">
+      <input class="un " type="text" align="center" placeholder="Username">
+      <input class="pass" type="password" align="center" placeholder="Password">
+      <a class="submit" align="center">Sign in</a>
+    </form>
+                
+</div>
+
+@endsection
---
+++ resources/views/auth/master/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title') - Laraguards</title>
+
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}"
+</head>
+<body>
+    
+    @yield('content')
+
+</body>
+</html>
