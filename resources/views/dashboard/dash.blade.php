7e092af7f76cac936581afcf0eb58b7f57ce035b
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-20 17:29:57 -0300

    :page_facing_up: Dashboard

---
+++ resources/views/dashboard/dash.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <style>
+        * {
+            font-family: sans-serif;
+        }
+        body {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <h1>Dashboard</h1>
+</body>
+</html>
