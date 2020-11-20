9a5e68a6b24d012bb5bb78f496a0de52b444db51
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-20 18:48:55 -0300

    :sparkles: Changed the dash route to view type

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,7 @@ use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'auth.login')->name('login.form')->middleware('guest');
-Route::post('/', [
-    LoginController::class, 'doLogin'
-    ])->name('login');
+Route::post('/', [LoginController::class, 'doLogin'])->name('login');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/dashboard', function() {
-    return view('dashboard.dash');
-})->middleware('auth:web');
-
-Route::get('/logout', [
-    LoginController::class, 'logout'
-    ])->name('logout');
+Route::view('/dashboard', 'dashboard.dash')->middleware('auth:web');
