1cad64d37253727aae55d30cdbbb4e6cf92e88f1
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-20 22:32:15 -0300

    :wrench: Configured password column

--- database/migrations/2020_11_20_015531_create_students_table.php
+++ database/migrations/2020_11_20_015531_create_students_table.php
@@ -18,7 +18,7 @@ class CreateStudentsTable extends Migration
 
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('password');
+            $table->string('password')->nullable();
 
             $table->timestamps();
         });
