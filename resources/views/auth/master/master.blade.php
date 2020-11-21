3f8722755c90b301f0522986219599bcbd15c778
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-20 22:38:16 -0300

    :bug: Closing link tag in master template

--- resources/views/auth/master/master.blade.php
+++ resources/views/auth/master/master.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - Laraguards</title>
 
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}"
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
 </head>
 <body>
     
