9fff6a1193eb998d18c7879ea429cddbbebe4bfd
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-21 18:24:55 -0300

    :recycle: Removed providers responsability

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Student;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
@@ -27,36 +24,6 @@ class LoginController extends Controller
     {
         Auth::logout();
         
-        return redirect('/');
-    }
-
-    public function redirectToProvider()
-    {
-        return Socialite::driver('google')->redirect();
-    }
-
-    public function handleProviderCallback()
-    {
-        try {
-            $user = Socialite::driver('google')->user();
-        } catch (\Exception $e) {
-            return redirect('/');
-        }
-
-        $existingUser = Student::where('email', $user->email)->first();
-
-        if ($existingUser) {
-            Auth::login($existingUser, true);
-        } else {
-            $newUser = new Student;
-            $newUser->name = $user->name;
-            $newUser->email = $user->email;
-            $newUser->google_id = $user->id;
-            $newUser->save();
-
-            Auth::login($newUser, true);
-        }
-
         return redirect('/');
     }
 }
