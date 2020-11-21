8ddd4e667de1ea57e6690c74c98a5e7563521109
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-20 21:27:18 -0300

    :wrench: Socialite configuration

--- config/services.php
+++ config/services.php
@@ -2,17 +2,11 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'), 
+        'redirect' => env('GOOGLE_REDIRECT')
+    ],
 
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
