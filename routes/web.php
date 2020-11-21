c7be6b6ebec653623c0e65a84a9bb24e4a9c0031
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-20 22:35:02 -0300

    :sparkles: Provider routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'auth.login')->name('login.form')->middleware('guest');
@@ -8,3 +9,8 @@ Route::post('/', [LoginController::class, 'doLogin'])->name('login');
 Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::view('/dashboard', 'dashboard.dash')->middleware('auth:web');
+
+Route::get('/redirect', [LoginController::class, 'redirectToProvider'])
+    ->name('redirect');
+Route::get('/callback', [LoginController::class, 'handleProviderCallback'])
+    ->name('provider.callback');
