28c233d802fb66263dfbff55f68984f26a77f8d3
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-20 22:34:16 -0300

    :lock: Using authenticatable in Student class

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 
-class Student extends Model
+class Student extends Model implements Authenticatable
 {
     use HasFactory;
+    use AuthenticableTrait;
 }
