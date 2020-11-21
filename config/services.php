aee8ad76351da77e06e972b1317cf962dc33148b
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-21 18:41:34 -0300

    :wrench: Github service driver

--- config/services.php
+++ config/services.php
@@ -8,6 +8,12 @@ return [
         'redirect' => env('GOOGLE_REDIRECT')
     ],
 
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'), 
+        'redirect' => env('GITHUB_REDIRECT')
+    ],
+
     'mailgun' => [
         'domain' => env('MAILGUN_DOMAIN'),
         'secret' => env('MAILGUN_SECRET'),
