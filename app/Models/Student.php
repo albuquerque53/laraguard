b126013f64be510b3a64a2dde0933cc4c0cef872
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-26 16:19:45 -0300

    :recycle: Improved student class code

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\{ Factories\HasFactory, Model };
 use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Notifications\Notifiable;
 
 class Student extends Model implements Authenticatable
 {
-    use HasFactory;
-    use AuthenticableTrait;
+    use HasFactory, AuthenticableTrait, Notifiable;
 }
