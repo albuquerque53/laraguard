b4c72dea8ce9d381dd590ec25778db483cf0c988
albuquerque53 <gabrielalbuquerque-ads@hotmail.com>
2020-11-27 02:16:15 -0300

    :wrench: Configured password reset table

--- config/auth.php
+++ config/auth.php
@@ -111,7 +111,12 @@ return [
             'expire' => 60,
             'throttle' => 60,
         ],*/
-        
+       'students' => [
+           'provider' => 'students',
+           'table' => 'password_resets',
+           'expire' => 60,
+           'throttle' => 60
+       ]
     ],
 
     /*
